dd146b1216cc6b6dc20affbd52861662f5d5589a
Andrew Zitek <zitek936@gmail.com>
2021-02-17 18:35:40 -0500

    make compile

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CXX=g++
 RM=rm -f
 CPPFLAGS=-g -Wall -std=c++11
 
-SRCS=cipher_algorithms.cc cipher_crackers.cc dictionaries.cc distance_measures.cc frequency_analyzers.cc key_length_finders.cc
+SRCS=cipher_algorithms_test.cc cipher_algorithms.cc cipher_crackers.cc dictionaries.cc distance_measures.cc frequency_analyzers.cc key_length_finders.cc
 OBJS=$(subst .cc,.o,$(SRCS))
 
 all: $(OBJS)
--- distance_measures.cc
+++ distance_measures.cc
@@ -1,23 +1,21 @@
-
+#include <string>
+#include <vector>
 
 // https://gist.github.com/TheRayTracer/2644387
-size_t levenshtein_distance(const char * s, size_t n, const char * t, size_t m) {
-  ++n;
-  ++m;
-  size_t * d = new size_t[n * m];
-  memset(d, 0, sizeof(size_t) * n * m);
-  for (size_t i = 1, im = 0; i < m; ++i, ++im) {
-    for (size_t j = 1, jn = 0; j < n; ++j, ++jn) {
+int levenshtein_distance(std::string s, std::string t) {
+  int n = s.length() + 1;
+  int m = t.length() + 1;
+  std::vector<int> d(n * m, 0);
+  for (int i = 1, im = 0; i < m; ++i, ++im) {
+    for (int j = 1, jn = 0; j < n; ++j, ++jn) {
       if (s[jn] == t[im]) {
         d[(i * n) + j] = d[((i - 1) * n) + (j - 1)];
       } else {
-        d[(i * n) + j] = min(d[(i - 1) * n + j] + 1, /* A deletion. */
-          min(d[i * n + (j - 1)] + 1, /* An insertion. */
+        d[(i * n) + j] = std::min(d[(i - 1) * n + j] + 1, /* A deletion. */
+          std::min(d[i * n + (j - 1)] + 1, /* An insertion. */
             d[(i - 1) * n + (j - 1)] + 1)); /* A substitution. */
       }
     }
   }
-  size_t r = d[n * m - 1];
-  delete[] d;
-  return r;
+  return d[n * m - 1];
 }
