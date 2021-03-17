e014a775576663be79abbe737d6d97cb2f6d262a
unknown <sonymaharjan04@gmail.com>
2021-03-17 18:30:53 -0500

    code for calculating execution time

--- __TESTS__/run_tests.cc
+++ __TESTS__/run_tests.cc
@@ -3,11 +3,21 @@
 #include "distance_measures_tests.hh"
 #include "frequency_analyzers_tests.hh"
 #include "key_length_finders_tests.hh"
+#include <iostream>
+#include <chrono>
+using namespace std::chrono;
 
 int main() {
+  auto start = high_resolution_clock::now();
+  
   cipher_algorithms_tests();
   distance_measures_tests();
   frequency_analyzers_tests();
   key_length_finders_tests();
   cipher_crackers_tests();
+  
+  auto end = high_resolution_clock::now();
+  double runtime = duration_cast<seconds>(end - start).count();
+  
+  std::cout << "The runtime is:" << runtime/60 << " mins" << std::endl;
 }
