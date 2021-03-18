3025565e7177c7ff9423ed055ce65b513f882e2d
Andrew Zitek <zitek936@gmail.com>
2021-03-18 14:41:52 -0400

    check all shift values

--- __TESTS__/run_tests.cc
+++ __TESTS__/run_tests.cc
@@ -9,15 +9,15 @@ using namespace std::chrono;
 
 int main() {
   auto start = high_resolution_clock::now();
-  
+
   cipher_algorithms_tests();
   distance_measures_tests();
   frequency_analyzers_tests();
   key_length_finders_tests();
   cipher_crackers_tests();
-  
+
   auto end = high_resolution_clock::now();
   double runtime = duration_cast<seconds>(end - start).count();
-  
+
   std::cout << "The runtime is:" << runtime/60 << " mins" << std::endl;
 }
